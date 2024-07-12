26fe2aa6e10ed2e7047a1b24dcff13c6f85ab5de
anggraininova <105601196+anggraininova@users.noreply.github.com>
2024-07-12 13:03:45 +0700

    Update img_classification.py

--- img_classification.py
+++ img_classification.py
@@ -1,30 +1,43 @@
-import keras
-from PIL import Image, ImageOps
+from keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
 
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
 
-def teachable_machine_classification(img, weights_file):
-    # Load the model
-    model = keras.models.load_model(weights_file)
+# Load the model
+model = load_model("keras_Model.h5", compile=False)
 
-    # Create the array of the right shape to feed into the keras model
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    image = img
-    #image sizing
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
 
-    #turn the image into a numpy array
-    image_array = np.asarray(image)
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-    # Load the image into the array
-    data[0] = normalized_image_array
+# Replace this with the path to your image
+image = Image.open("<IMAGE_PATH>").convert("RGB")
 
-    # run the inference
-    prediction = model.predict(data)
-    index = np.argmax(prediction)
-    teachable_machine_classification.confidence_score = round(100 * (np.max(prediction[0])),2)
-    return index
-     # return position of the highest probability
+# resizing the image to be at least 224x224 and then cropping from the center
+size = (224, 224)
+image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+# turn the image into a numpy array
+image_array = np.asarray(image)
+
+# Normalize the image
+normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+# Load the image into the array
+data[0] = normalized_image_array
+
+# Predicts the model
+prediction = model.predict(data)
+index = np.argmax(prediction)
+class_name = class_names[index]
+confidence_score = prediction[0][index]
+
+# Print prediction and confidence score
+print("Class:", class_name[2:], end="")
+print("Confidence Score:", confidence_score)
